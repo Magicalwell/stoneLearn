346aa7ad52dc99f333c10024e889c7669e361d8b
threestone <812081428@qq.com>
2023-03-06 23:54:08 +0800

    feat: 完善组件的另一种方法extend，完善注释

--- Vue/src/core/global-api/extend.js
+++ Vue/src/core/global-api/extend.js
@@ -1,6 +1,9 @@
+import { mergeOptions, extend } from '../util/index'
+import { ASSET_TYPES } from 'shared/constants'
 export function initExtend(Vue) {
   Vue.cid = 0
   let cid = 1
+  // extend是一种挂载组件的方式 类似于components，但是它会创建一个新的子类
   Vue.extend = function (extendOptions) {
     const Super = this
     const SuperId = Super.cid
@@ -10,7 +13,49 @@ export function initExtend(Vue) {
     }
     const name = extendOptions.name || Super.options.name
     const Sub = function VueComponent(options) {
-      this._init(options)
+      this._init(options) // 给子类调用初始化方法，但走到这一步时，this指向子类本身，但本身还没任何方法
     }
+    // 到此后续就和本身vue的初始化一样，拓展vue构造函数，挂载各种方法，合并options，初始化props等
+    Sub.prototype = Object.create(Super.prototype)
+    Sub.prototype.constructor = Sub
+    // 继承父类的方法和属性
+    Sub.cid = cid++
+    Sub.options = mergeOptions(Super.options, extendOptions)
+    Sub['super'] = Super
+    // 到这里Sub的options已经合并了options，下面可以直接使用
+    if (Sub.options.props) {
+      initProps(Sub)
+    }
+    if (Sub.options.computed) {
+      initComputed(Sub)
+    }
+    // 把父类的方法挂给子类一份
+    Sub.extend = Super.extend
+    Sub.mixin = Super.mixin
+    Sub.use = Super.use
+    ASSET_TYPES.forEach(function (type) {
+      Sub[type] = Super[type]
+    });
+    if (name) {
+      Sub.options.components[name] = Sub
+    }
+    Sub.superOptions = Super.options
+    Sub.extendOptions = extendOptions
+    Sub.sealedOptions = extend({}, Sub.options)
+    cachedCtors[SuperId] = Sub
+    return Sub
+  }
+}
+function initProps(Comp) {
+  const props = Comp.options.props
+  for (const key in props) {
+    proxy(Comp.prototype, `_props`, key)
+  }
+}
+
+function initComputed(Comp) {
+  const computed = Comp.options.computed
+  for (const key in computed) {
+    defineComputed(Comp.prototype, key, computed[key])
   }
 }
--- Vue/src/core/instance/init.js
+++ Vue/src/core/instance/init.js
@@ -10,6 +10,8 @@ export function initMixin(Vue) {
         if (options && options._isComponent) {
 
         } else {
+            // 这里选项合并的对象是 initglobalApi中创建的那个options，里面有components filter directive 和new vue时传入的选项
+            // 但刚刚initglobalApi的时候，提到了一个vue.extend的方法，这也是创建vue实例的方法之一，这里合并的时候就需要考虑到这种方式
             vm.$options = mergeOptions(resolveConstructorOptions(vm.constructor), options || {}, vm)
         }
         if (process.env.NODE_ENV !== 'production') {
@@ -26,6 +28,7 @@ export function resolveConstructorOptions(Ctor) {
     // 在上面merge的时候传入的参数是vm.constructor，constructor指向的是当前对象的构造函数 vm._proto_指向的是当前对象的原型，
     // prototype是构造函数的属性，指向它的原型
     let options = Ctor.options
+    // 不管是new出来 还是extend出来，都会有options，区别是new出来是自己的，extend出来是已经合并过的
     // 这里存在super的情况，并不是es6 class的那个super，而是在调用vue.extend的时候，会给实例新增一个属性，如下：
     // Vue.extend = function (extendOptions: Object): Function {
     //     ...
