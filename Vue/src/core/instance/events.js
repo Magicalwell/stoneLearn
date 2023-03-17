bd26e627cafff7a5ce5ba049b33af80e3c2cd307
ljing <ljing@pisx.com>
2023-03-17 15:54:38 +0800

    feat: 完善自定义事件的on once off emit的方法

--- Vue/src/core/instance/events.js
+++ Vue/src/core/instance/events.js
@@ -1,3 +1,6 @@
+import {
+  toArray, invokeWithErrorHandling
+} from '../util/index'
 let target
 function add(event, fn) {
   target.$on(event, fn)
@@ -24,4 +27,82 @@ export function updateComponentListeners(vm, listeners, oldListeners) {
   //这里的add,remove是之前在实例上挂载的$on $off方法,具体是在vue构造函数那里，会进行一些方法的混入,至于为什么不直接vm.$on，猜测是借鉴了单例模式，同一时间只会有一个父组件的子组件在创建
   updateListeners(listeners, oldListeners || {}, add, remove, createOnceHandler, vm)
   target = undefined
+}
+
+export function eventsMixin(Vue) {
+  const hookRE = /^hook:/  // 用于处理写在标签上的hook钩子
+  Vue.prototype.$on = function (event, fn) {
+    const vm = this
+    if (Array.isArray(event)) {
+      // 处理传入数组的情况，挨个遍历调用$On，这个$On就是 Vue.prototype.$on 
+      for (let i = 0, l = event.length; i < l; i++) {
+        vm.$on(event[i], fn)
+      }
+    } else {
+      // 这里保存事件，之前initEvents的时候给vm上面初始化了_events，在这里把对应事件的处理方法存放在数组中
+      (vm._events[event] || (vm._events[event] = [])).push(fn)
+      if (hookRE.test(event)) {
+        vm._hasHookEvent = true
+      }
+    }
+    return vm
+  }
+  Vue.prototype.$once = function (event, fn) {
+    // 处理只调用一次的事件，原理其实还是利用$on 只不过把fn手动封装一下，让它执行逻辑的时候先在events中删除自身
+    const vm = this
+    function on() {
+      vm.$off(event, on)
+      fn.apply(vm, arguments)
+    }
+    on.fn = fn
+    vm.$on(event, on)
+    return vm
+  }
+  Vue.prototype.$off = function (event, fn) {
+    const vm = this
+    if (!arguments.length) {
+      // 不传参数直接清空所有的事件
+      vm._events = Object.create(null)
+      return vm
+    }
+    if (Array.isArray(event)) {
+      // 数组入参挨个调用off
+      for (let i = 0, l = event.length; i < l; i++) {
+        vm.$off(event[i], fn)
+      }
+      return vm
+    }
+    const cbs = vm._events[event] // 此处获得的是对应event的回调函数list
+    if (!cbs) {
+      return vm
+    }
+    if (!fn) {
+      // 未指定具体删除哪一个回调函数，就把这个event底下的都清空
+      vm._events[event] = null
+      return vm
+    }
+    let cb
+    let i = cbs.length
+    while (i--) {
+      cb = cbs[i]
+      if (cb === fn || cb.fn === fn) {
+        cbs.splice(i, 1)
+        break
+      }
+    }
+    return vm
+  }
+  Vue.prototype.$emit = function (event) {
+    const vm = this
+    let cbs = vm._events[event]
+    if (cbs) {
+      cbs = cbs.length > 1 ? toArray(cbs) : cbs
+      const args = toArray(arguments, 1)
+      const info = ''
+      for (let i = 0, l = cbs.length; i < l; i++) {
+        // 遍历对应事件底下的处理函数，这里用错误处理函数包裹
+        invokeWithErrorHandling(cbs[i], vm, args, vm, info)
+      }
+    }
+  }
 }
--- Vue/src/core/instance/index.js
+++ Vue/src/core/instance/index.js
@@ -1,9 +1,11 @@
 import { initMixin } from "./init";
 import { stateMixin } from './state'
+import { eventsMixin } from './events'
 function Vue(options) {
     let a = 1
     this._init(options)
 }
 initMixin(Vue) // 给构造函数拓展init方法
-stateMixin(Vue)
+stateMixin(Vue) // 给构造函数原型上添加set del方法，并且添加watch
+eventsMixin(Vue) // 给原型上挂载on once emit off等方法
 export default Vue
